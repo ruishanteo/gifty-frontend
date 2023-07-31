003fe4b8c470032751ab3a607246c42c9a8184ff
ruishanteo <ruishan.teo03@gmail.com>
2023-07-31 08:19:07 +0800

    fixed scroll view and keyboard avoiding

--- pages/Login.js
+++ pages/Login.js
@@ -1,56 +1,65 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  Dimensions,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
 import { Button, Text, TextInput, useTheme } from "react-native-paper";
 
 import { Image } from "@rneui/themed";
 
 import logo from "../assets/logo.png";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export function Login({ navigation }) {
   const theme = useTheme();
+  const windowHeight = Dimensions.get("window").height;
 
   return (
-    <View style={styles.container}>
-      <Image
-        containerStyle={{
-          width: "40%",
-          aspectRatio: 1,
-        }}
-        source={{ uri: Image.resolveAssetSource(logo).uri }}
-      />
-      <Text variant="headlineMedium">Welcome back</Text>
-      <View style={{ width: "75%" }}>
-        <TextInput mode="outlined" placeholder="Email" />
-        <TextInput mode="outlined" placeholder="Password" />
-      </View>
-      <Button
-        mode="contained"
-        textColor={theme.colors.surface}
-        buttonColor={theme.colors.secondary}
-        style={{
-          marginTop: 10,
-          marginBottom: 15,
-        }}
-      >
-        Login
-      </Button>
-      <Text>Don't have an account?</Text>
-      <Button
-        style={{ marginTop: -5 }}
-        onPress={() => navigation.navigate("Register")}
-      >
-        Register
-      </Button>
-    </View>
+    <ScrollView>
+      <KeyboardAvoidingView behavior="height" style={styles.container}>
+        <Image
+          containerStyle={{
+            width: "40%",
+            aspectRatio: 1,
+          }}
+          source={{ uri: Image.resolveAssetSource(logo).uri }}
+        />
+        <Text variant="headlineMedium">Welcome back</Text>
+        <View style={{ width: "75%" }}>
+          <TextInput mode="outlined" placeholder="Email" />
+          <TextInput mode="outlined" placeholder="Password" />
+        </View>
+        <Button
+          mode="contained"
+          textColor={theme.colors.surface}
+          buttonColor={theme.colors.secondary}
+          style={{
+            marginTop: 10,
+            marginBottom: 15,
+          }}
+        >
+          Login
+        </Button>
+        <Text>Don't have an account?</Text>
+        <Button
+          style={{ marginTop: -5 }}
+          onPress={() => navigation.navigate("Register")}
+        >
+          Register
+        </Button>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
+    marginTop: 40,
   },
 });
--- pages/Register.js
+++ pages/Register.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Button, Text, TextInput, useTheme } from "react-native-paper";
 import {
   DatePickerModal,
@@ -32,78 +38,80 @@ export function Register({ navigation }) {
   registerTranslation("en-GB", enGB);
 
   return (
-    <View style={styles.container}>
-      <Image
-        containerStyle={{
-          width: "40%",
-          aspectRatio: 1,
-        }}
-        source={{ uri: Image.resolveAssetSource(logo).uri }}
-      />
-      <Text variant="headlineMedium">Get Started</Text>
-      <View style={{ width: "75%" }}>
-        <TextInput mode="outlined" placeholder="Username" />
-        <TextInput mode="outlined" placeholder="Email" />
-        <TextInput mode="outlined" placeholder="Password" />
-        <TextInput mode="outlined" placeholder="Confirm Password" />
-        <TouchableOpacity onPress={() => setOpenDatePicker(true)}>
-          <View
-            style={{
-              width: "100%",
-              height: 50,
-              marginTop: 6,
-
-              borderColor: "grey",
-              borderWidth: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 5,
-            }}
-          >
-            {date ? (
-              <Text>{moment(date).format("DD MMM YYYY")}</Text>
-            ) : (
-              <Text>Please select your birthday</Text>
-            )}
-          </View>
-        </TouchableOpacity>
-        <DatePickerModal
-          locale="en-GB"
-          mode="single"
-          visible={openDatePicker}
-          onDismiss={onDismissSingle}
-          date={date}
-          onConfirm={onConfirmSingle}
+    <ScrollView>
+      <KeyboardAvoidingView behavior="height" style={styles.container}>
+        <Image
+          containerStyle={{
+            width: "40%",
+            aspectRatio: 1,
+          }}
+          source={{ uri: Image.resolveAssetSource(logo).uri }}
         />
-      </View>
-      <Button
-        mode="contained"
-        textColor={theme.colors.surface}
-        buttonColor={theme.colors.secondary}
-        style={{
-          marginTop: 10,
-          marginBottom: 15,
-        }}
-      >
-        Sign up
-      </Button>
-      <Text>Already have an account?</Text>
-      <Button
-        style={{ marginTop: -5 }}
-        onPress={() => navigation.navigate("Login")}
-      >
-        Login
-      </Button>
-    </View>
+        <Text variant="headlineMedium">Get Started</Text>
+        <View style={{ width: "75%" }}>
+          <TextInput mode="outlined" placeholder="Username" />
+          <TextInput mode="outlined" placeholder="Email" />
+          <TextInput mode="outlined" placeholder="Password" />
+          <TextInput mode="outlined" placeholder="Confirm Password" />
+          <TouchableOpacity onPress={() => setOpenDatePicker(true)}>
+            <View
+              style={{
+                width: "100%",
+                height: 50,
+                marginTop: 6,
+
+                borderColor: "grey",
+                borderWidth: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: 5,
+              }}
+            >
+              {date ? (
+                <Text>{moment(date).format("DD MMM YYYY")}</Text>
+              ) : (
+                <Text>Please select your birthday</Text>
+              )}
+            </View>
+          </TouchableOpacity>
+          <DatePickerModal
+            locale="en-GB"
+            mode="single"
+            visible={openDatePicker}
+            onDismiss={onDismissSingle}
+            date={date}
+            onConfirm={onConfirmSingle}
+          />
+        </View>
+        <Button
+          mode="contained"
+          textColor={theme.colors.surface}
+          buttonColor={theme.colors.secondary}
+          style={{
+            marginTop: 10,
+            marginBottom: 15,
+          }}
+        >
+          Sign up
+        </Button>
+        <Text>Already have an account?</Text>
+        <Button
+          style={{ marginTop: -5 }}
+          onPress={() => navigation.navigate("Login")}
+        >
+          Login
+        </Button>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
+    marginTop: 40,
   },
 });
