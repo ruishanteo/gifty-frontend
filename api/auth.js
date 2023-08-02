b0a1c4786d49cbb848f4cc3089f2ab3428d83df3
mfjkri <muhammadfikri6844@gmail.com>
2023-08-02 12:20:36 +0800

    integrated register with api

--- api/auth.js
+++ api/auth.js
@@ -25,3 +25,29 @@ export function useLogin() {
       });
   };
 }
+
+export function useRegister() {
+  const { publicAxios } = useAxios();
+  const { setAccessToken } = useAuth();
+  const { showNotification } = useNotification();
+
+  return async (registerRequest) => {
+    return await publicAxios
+      .post("public/auth/register", registerRequest)
+      .then((res) => {
+        setAccessToken(res.data.data.token);
+        showNotification({
+          title: "Register Successful",
+          description: "Welcome to gifty!",
+          type: "success",
+        });
+      })
+      .catch((error) => {
+        showNotification({
+          title: "Registration Failed",
+          description: error.response.data?.message || "Please try again.",
+          type: "error",
+        });
+      });
+  };
+}
--- pages/Register.js
+++ pages/Register.js
@@ -6,19 +6,40 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { Button, Text, TextInput, useTheme } from "react-native-paper";
+import {
+  Button,
+  HelperText,
+  Text,
+  TextInput,
+  useTheme,
+} from "react-native-paper";
 import {
   DatePickerModal,
   enGB,
   registerTranslation,
 } from "react-native-paper-dates";
-import moment from "moment";
 import { Image } from "@rneui/themed";
+import moment from "moment";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 import logo from "../assets/logo.png";
+import { useRegister } from "../api/auth";
+
+const registerValidationSchema = Yup.object().shape({
+  username: Yup.string().required("Username is Required"),
+  email: Yup.string()
+    .email("Please enter valid email")
+    .required("Email Address is Required"),
+  password: Yup.string().required("Password is required"),
+  confirmPassword: Yup.string()
+    .required("Password is required")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
 
 export function Register({ navigation }) {
   const theme = useTheme();
+  const register = useRegister();
 
   const [date, setDate] = React.useState(undefined);
   const [openDatePicker, setOpenDatePicker] = React.useState(false);
@@ -48,52 +69,133 @@ export function Register({ navigation }) {
           source={{ uri: Image.resolveAssetSource(logo).uri }}
         />
         <Text variant="headlineMedium">Get Started</Text>
-        <View style={{ width: "75%" }}>
-          <TextInput mode="outlined" placeholder="Username" />
-          <TextInput mode="outlined" placeholder="Email" />
-          <TextInput mode="outlined" placeholder="Password" />
-          <TextInput mode="outlined" placeholder="Confirm Password" />
-          <TouchableOpacity onPress={() => setOpenDatePicker(true)}>
-            <View
-              style={{
-                width: "100%",
-                height: 50,
-                marginTop: 6,
-
-                borderColor: "grey",
-                borderWidth: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                borderRadius: 5,
-              }}
-            >
-              {date ? (
-                <Text>{moment(date).format("DD MMM YYYY")}</Text>
-              ) : (
-                <Text>Please select your birthday</Text>
-              )}
-            </View>
-          </TouchableOpacity>
-          <DatePickerModal
-            locale="en-GB"
-            mode="single"
-            visible={openDatePicker}
-            onDismiss={onDismissSingle}
-            date={date}
-            onConfirm={onConfirmSingle}
-          />
-        </View>
-        <Button
-          mode="contained"
-          textColor={theme.colors.surface}
-          buttonColor={theme.colors.secondary}
-          style={{
-            marginTop: 10,
-            marginBottom: 15,
+        <Formik
+          validationSchema={registerValidationSchema}
+          initialValues={{
+            username: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
           }}
+          onSubmit={(values) => register({ ...values, birthday: date })}
         >
-          Sign up
-        </Button>
+          {({
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+          }) => (
+            <>
+              <View style={{ width: "75%" }}>
+                <TextInput
+                  mode="outlined"
+                  label="Username"
+                  placeholder="Username"
+                  onChangeText={handleChange("username")}
+                  onBlur={handleBlur("username")}
+                  value={values.username}
+                  error={errors.username && touched.username}
+                />
+                <HelperText
+                  type="error"
+                  visible={errors.username && touched.username}
+                >
+                  {errors.username}
+                </HelperText>
+                <TextInput
+                  mode="outlined"
+                  label="Email"
+                  placeholder="Email"
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                  value={values.email}
+                  error={errors.email && touched.email}
+                />
+                <HelperText
+                  type="error"
+                  visible={errors.email && touched.email}
+                >
+                  {errors.email}
+                </HelperText>
+                <TextInput
+                  mode="outlined"
+                  label="Password"
+                  placeholder="Password"
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                  value={values.password}
+                  error={errors.password && touched.password}
+                />
+                <HelperText
+                  type="error"
+                  visible={errors.password && touched.password}
+                >
+                  {errors.password}
+                </HelperText>
+                <TextInput
+                  mode="outlined"
+                  label="Confirm Password"
+                  placeholder="Confirm Password"
+                  onChangeText={handleChange("confirmPassword")}
+                  onBlur={handleBlur("confirmPassword")}
+                  value={values.confirmPassword}
+                  error={errors.confirmPassword && touched.confirmPassword}
+                />
+                <HelperText
+                  type="error"
+                  visible={errors.confirmPassword && touched.confirmPassword}
+                >
+                  {errors.confirmPassword}
+                </HelperText>
+                <TouchableOpacity onPress={() => setOpenDatePicker(true)}>
+                  <View
+                    style={{
+                      width: "100%",
+                      height: 50,
+                      marginTop: 6,
+
+                      borderColor: "grey",
+                      borderWidth: 1,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      borderRadius: 5,
+                    }}
+                  >
+                    {date ? (
+                      <Text>{moment(date).format("DD MMM YYYY")}</Text>
+                    ) : (
+                      <Text>Please select your birthday</Text>
+                    )}
+                  </View>
+                </TouchableOpacity>
+                <DatePickerModal
+                  locale="en-GB"
+                  mode="single"
+                  visible={openDatePicker}
+                  onDismiss={onDismissSingle}
+                  date={date}
+                  onConfirm={onConfirmSingle}
+                />
+              </View>
+              <Button
+                onPress={handleSubmit}
+                disabled={isSubmitting}
+                mode="contained"
+                textColor={theme.colors.surface}
+                buttonColor={theme.colors.secondary}
+                style={{
+                  marginTop: 10,
+                  marginBottom: 15,
+                }}
+              >
+                Sign up
+              </Button>
+            </>
+          )}
+        </Formik>
         <Text>Already have an account?</Text>
         <Button
           style={{ marginTop: -5 }}
