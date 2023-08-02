55327c570cad15fe2964320f07cc07f9be295eef
mfjkri <muhammadfikri6844@gmail.com>
2023-08-02 15:07:14 +0800

    integrated edit profile with api

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,9 @@
-import { useAuth, useAxios, useNotification } from "../providers/hooks";
+import {
+  useAuth,
+  useAxios,
+  useNotification,
+  useUser,
+} from "../providers/hooks";
 
 export function useLogin() {
   const { publicAxios } = useAxios();
@@ -7,18 +12,18 @@ export function useLogin() {
 
   return async (loginRequest) => {
     return await publicAxios
-      .post("public/auth/login", loginRequest)
+      .post("auth/login", loginRequest)
       .then((res) => {
         setAccessToken(res.data.data.token);
         showNotification({
-          title: "Login Successful",
-          description: "Welcome to gifty!",
+          title: "Login successful",
+          description: `Welcome back ${res.data.data.user.username}!`,
           type: "success",
         });
       })
       .catch((error) => {
         showNotification({
-          title: "Login Failed",
+          title: "Login failed",
           description: error.response.data?.message || "Please try again.",
           type: "error",
         });
@@ -33,18 +38,45 @@ export function useRegister() {
 
   return async (registerRequest) => {
     return await publicAxios
-      .post("public/auth/register", registerRequest)
+      .post("auth/register", registerRequest)
       .then((res) => {
         setAccessToken(res.data.data.token);
         showNotification({
-          title: "Register Successful",
+          title: "Register successful",
           description: "Welcome to gifty!",
           type: "success",
         });
       })
       .catch((error) => {
         showNotification({
-          title: "Registration Failed",
+          title: "Registration failed",
+          description: error.response.data?.message || "Please try again.",
+          type: "error",
+        });
+      });
+  };
+}
+
+export function useUpdateUser() {
+  const { protectedAxios } = useAxios();
+  const { reloadUser } = useUser();
+  const { showNotification } = useNotification();
+
+  return async (updateRequest) => {
+    return await protectedAxios
+      .put("auth/updateUser", updateRequest)
+      .then((res) => {
+        reloadUser();
+        showNotification({
+          title: "Update Successful",
+          description: "Updated your account information",
+          type: "success",
+        });
+        return res;
+      })
+      .catch((error) => {
+        showNotification({
+          title: "Update failed",
           description: error.response.data?.message || "Please try again.",
           type: "error",
         });
