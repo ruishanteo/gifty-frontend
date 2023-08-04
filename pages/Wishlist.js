6bd2791c3175294452c6cef801ca0de8e00b8613
ruishanteo <ruishan.teo03@gmail.com>
2023-08-04 08:06:26 +0800

    updated wishlist and added list view

--- pages/Wishlist.js
+++ pages/Wishlist.js
@@ -1,18 +1,84 @@
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Button, useTheme } from "react-native-paper";
+import { ListItem, SearchBar } from "@rneui/themed";
 
-export function Wishlist() {
-  return (
-    <View style={styles.container}>
-      <Text>Wishlist</Text>
-    </View>
+export const Wishlist = ({ navigation }) => {
+  const theme = useTheme();
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const onChangeSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const friends = [
+    { name: "Amy", id: 1 },
+    { name: "BooBoo", id: 2 },
+    { name: "Charlie", id: 3 },
+    { name: "Donald", id: 4 },
+  ];
+
+  const Item = ({ item }) => (
+    <ListItem.Swipeable
+      bottomDivider
+      onPress={() => navigation.navigate("WishResult", { friend: item })}
+      rightContent={(reset) => (
+        <Button
+          onPress={() => console.log(reset)}
+          icon="delete"
+          buttonColor={theme.colors.quaternary}
+          textColor={theme.colors.surface}
+          style={{
+            height: "100%",
+            justifyContent: "center",
+          }}
+        >
+          Delete
+        </Button>
+      )}
+      containerStyle={{
+        backgroundColor: theme.colors.background,
+        borderColor: theme.colors.secondary,
+      }}
+    >
+      <ListItem.Content>
+        <ListItem.Title style={{ color: theme.colors.font }}>
+          {item.name}
+        </ListItem.Title>
+      </ListItem.Content>
+      <ListItem.Chevron />
+    </ListItem.Swipeable>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  return (
+    <SafeAreaView>
+      <View>
+        <SearchBar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+          containerStyle={{
+            backgroundColor: "transparent",
+            borderBottomColor: "transparent",
+            borderTopColor: "transparent",
+          }}
+          inputContainerStyle={{ backgroundColor: theme.colors.primary }}
+          round={true}
+        />
+
+        <FlatList
+          data={friends}
+          renderItem={({ item }) => <Item item={item} />}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => <View style={{ height: 2 }} />}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
