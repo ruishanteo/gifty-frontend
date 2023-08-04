0b8b1975a5f5ebc0d2e602c046b0b00752beef8d
ruishanteo <ruishan.teo03@gmail.com>
2023-08-04 08:04:37 +0800

    added delete account button

--- subpages/Settings.js
+++ subpages/Settings.js
@@ -1,15 +1,18 @@
-import { ListItem } from "@rneui/themed";
 import React from "react";
-import { View } from "react-native";
-import { Button, Switch, useTheme } from "react-native-paper";
-
+import { StyleSheet, View } from "react-native";
+import { Button, Switch, Text, useTheme } from "react-native-paper";
+import { ListItem } from "@rneui/themed";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Modal from "react-native-modal";
+
 import { useAuth } from "../providers/hooks";
 import Layout from "../components/Layout";
 
 export const Settings = ({ navigation }) => {
   const theme = useTheme();
   const { logout } = useAuth();
+
+  const [open, setOpen] = React.useState(false);
 
   return (
     <SafeAreaView>
@@ -78,7 +81,23 @@ export const Settings = ({ navigation }) => {
 
           <ListItem
             bottomDivider
-            onPress={() => console.log("terms and conditions")}
+            onPress={() => setOpen(true)}
+            style={{ width: "100%" }}
+            containerStyle={{
+              backgroundColor: theme.colors.background,
+              borderColor: theme.colors.secondary,
+            }}
+          >
+            <ListItem.Content>
+              <ListItem.Title style={{ color: "red" }}>
+                Delete Account
+              </ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron />
+          </ListItem>
+
+          <ListItem
+            bottomDivider
             style={{ width: "100%" }}
             containerStyle={{
               backgroundColor: theme.colors.background,
@@ -95,8 +114,79 @@ export const Settings = ({ navigation }) => {
               </Button>
             </ListItem.Content>
           </ListItem>
+
+          <Modal
+            animationType="slide"
+            visible={open}
+            onRequestClose={() => setOpen(false)}
+          >
+            <View style={styles.centeredView}>
+              <View
+                style={[
+                  styles.modalView,
+                  { backgroundColor: theme.colors.tertiary },
+                ]}
+              >
+                <Layout
+                  title="Are you sure?"
+                  onAction={() => setOpen(false)}
+                  iconName="close"
+                >
+                  <View
+                    style={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: 20,
+                    }}
+                  >
+                    <Text>This action is irreversible.</Text>
+                    <View flexDirection="row" style={{ gap: 15 }}>
+                      <Button
+                        buttonColor={theme.colors.quaternary}
+                        textColor={theme.colors.background}
+                        icon="close"
+                        onPress={() => setOpen(false)}
+                      >
+                        Cancel
+                      </Button>
+                      <Button
+                        buttonColor={theme.colors.quaternary}
+                        textColor={theme.colors.background}
+                        icon="check"
+                        onPress={() => console.log("delete account")}
+                      >
+                        Confirm
+                      </Button>
+                    </View>
+                  </View>
+                </Layout>
+              </View>
+            </View>
+          </Modal>
         </View>
       </Layout>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: "100%",
+  },
+});
