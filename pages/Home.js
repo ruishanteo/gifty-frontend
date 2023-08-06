61990ed5a03d3cc1005cbb43e307a93bc4ecd7f0
ruishanteo <ruishan.teo03@gmail.com>
2023-08-06 09:03:11 +0800

    updated home page with font

--- pages/Home.js
+++ pages/Home.js
@@ -1,18 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Text, useTheme } from "react-native-paper";
+import { Image } from "@rneui/themed";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import logo from "../assets/logo.png";
 
 export function Home() {
+  const theme = useTheme();
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView
+      style={{ backgroundColor: theme.colors.tertiary, paddingBottom: -20 }}
+    >
+      <View style={[styles.container]}>
+        <Image
+          containerStyle={{ width: 50, aspectRatio: 1 }}
+          source={{ uri: Image.resolveAssetSource(logo).uri }}
+        />
+        <Text style={{ fontFamily: "BestMoment" }} variant="displayLarge">
+          gifty
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+    height: 70,
+    flexDirection: "row",
+    gap: 15,
   },
 });
