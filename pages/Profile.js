4e3c7dd7766419dd3622b7d9e4a2ccd35cfb7a23
Rui Shan <ruishan.teo03@gmail.com>
2023-08-29 09:34:16 +0800

    Fix icon

--- pages/Profile.js
+++ pages/Profile.js
@@ -157,7 +157,7 @@ export function Profile({ navigation }) {
               style={{ padding: 10, alignItems: "center" }}
             >
               <MaterialCommunityIcons
-                name="gift-outline"
+                name="bookmark-outline"
                 color={theme.colors.secondary}
                 size={25}
               />
